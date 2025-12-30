33a2d7ae31cb043900bcbd9aa655219056711f87
HoualefAkram <akram1232004@gmail.com>
2025-12-30 05:27:56 +0100

    dynamic plotting

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from models.model import Model
 from models.layer import Input, Dense
 from models.activation import SoftPlus, Linear
 
-x_train = [[0], [0.5], [1]]
+x_train = [[0], [0.9], [1]]
 y_train = [[0], [1], [0]]
 SHOW_GRAPH = True
 
@@ -11,6 +11,7 @@ model = Model()
 
 model.add_layer(layer=Input(number_features=1))  # input layer
 model.add_layer(layer=Dense(2, activation=SoftPlus()))  # hidden layer 1
+model.add_layer(layer=Dense(2, activation=SoftPlus()))  # hidden layer 2
 model.add_layer(layer=Dense(1, activation=Linear()))  # output layer
 
 
@@ -20,7 +21,15 @@ model.fit(x_train=x_train, y_train=y_train, iterations=20000, learning_rate=0.1)
 if SHOW_GRAPH:
     from matplotlib import pyplot as plt
 
-    x_axis = [i / 10 for i in range(11)]
+    x_train_max = max([x[0] for x in x_train])
+    x_train_min = min([x[0] for x in x_train])
+    points_number = 10
+    step = (x_train_max - x_train_min) / (points_number - 1)
+    x_axis = []
+    current_val = x_train_min
+    for i in range(points_number):
+        x_axis.append(current_val)
+        current_val += step
     y_axis = [model.predict([x])[0] for x in x_axis]
 
     plt.plot(x_axis, y_axis)
